421c6402774f313ee8f870ba8fd6a8e6d2ac7592
evangelino777 <85470764+evangelino777@users.noreply.github.com>
2022-09-19 14:04:13 +0700

    mongoose 2-1

--- myApp.js
+++ myApp.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const schema = mongoose.schema;
 
-let Person;
+const personSchema = new schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
